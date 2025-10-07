7c105cfdd6350c2fbb1ac48ad50c909494dcee20
Gladwin Joy <gladu8908@gmail.com>
2025-10-07 15:57:23 +0530

    Refactor Introduction.py with structured sections

--- Introduction.py
+++ Introduction.py
@@ -1,3 +1,53 @@
-Running My first Python script
+#1.
+
+#Running My first PYTHON SCRIPT
 
 print("I Love Python")
+
+#2.
+#Python SYNTHAX
+
+#Pyhthon as calsulator
+
+print(19 + 17)
+print(99 - 12)
+
+
+#3.
+#more calculations
+
+print(9 * 2)
+print(120 / 12)
+
+#4
+#VARIABLES
+
+#Working With number Variables
+
+customer_age = 27
+account_balance = 120.50
+print(account_balance)
+
+
+#5
+#updating variable values
+
+account_balance = 20000.00
+is_millionaire = False
+
+
+account_balance = 1300000.00
+
+is_millionaire = True
+
+print(account_balance)
+print(is_millionaire)
+
+#6
+#Checking data types
+
+customer_age = 49
+account_balance = 120078.89
+is_millionaire = False
+
+print(type(is_millionaire))
